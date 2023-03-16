ef184600d10c60716a573b76a633d30855f4e9ec
godongs <asd236678@gmail.com>
2023-03-16 14:59:05 +0900

    add secure load

--- run.sh
+++ run.sh
@@ -14,4 +14,6 @@ sed 's/*/"/g' env.json >> secureFile.json
 # 그리고 {application_name} 부분을 실제 앱 이름으로 바꿔주시길 바랍니다. 보통 프로젝트 이름인데 헷갈리면
 # https://junghyun100.github.io/Gradle%EC%97%90%EC%84%9C-Jar-%ED%8C%8C%EC%9D%BC-%EC%83%9D%EC%84%B1%ED%95%98%EA%B8%B0/
 # java -Dserver.port=$PORT $JAVA_OPTS  -jar ./build/libs/{application_name}-0.0.1-SNAPSHOT.jar
+echo ${SECURE_FILE} >> env.json 
+sed 's/*/"/g' env.json >> secureFile.json
 java -Dserver.port=$PORT $JAVA_OPTS  -jar ./build/libs/demo-0.0.1-SNAPSHOT.jar
