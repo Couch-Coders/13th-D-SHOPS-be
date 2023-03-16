231aca0feb5709c50089f4d8d6905e06b9c7b00b
jayleeforever <jayforgood@gmail.com>
2023-03-16 17:48:29 +0900

    mod secureFile 설정병경

--- src/main/java/com/example/demo/config/FirebaseInitializer.java
+++ src/main/java/com/example/demo/config/FirebaseInitializer.java
@@ -21,7 +21,7 @@ public class FirebaseInitializer {
     public FirebaseApp firebaseApp() throws IOException {
         log.info("Initializing Firebase. ");
         FileInputStream serviceAccount =
-                new FileInputStream("firebase.json");
+                new FileInputStream("secureFile.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
