bcb0ac95c3f5a9c6d08669195a9c6f3f89ba9571
jayleeforever <jayforgood@gmail.com>
2023-04-06 01:14:55 +0900

    V1.11 prepare

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "V1.10(move near product to address)";
+        return "V1.11(add near url)";
     }
 
 }
